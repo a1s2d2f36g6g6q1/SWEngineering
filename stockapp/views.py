94f77fd26ef2454724c6bc2ab947342e6ab56a89
a1s2d2f36g6g6q1 <vrc.maya@chungbuk.ac.kr>
2024-12-07 17:23:06 +0900

    완료

--- stockapp/views.py
+++ stockapp/views.py
@@ -1,8 +1,7 @@
-from django.shortcuts import render
-
-# Create your views here.
-import matplotlib.pyplot as plt
 from django.shortcuts import render, HttpResponse
+import matplotlib
+matplotlib.use('Agg')  # GUI 백엔드 대신 Agg 백엔드 사용 (이미지 저장용)
+import matplotlib.pyplot as plt
 from sklearn import preprocessing
 import pandas as pd
 import requests
@@ -10,136 +9,106 @@ import os
 import json
 import numpy as np
 from statsmodels.tsa.arima.model import ARIMA
+from io import BytesIO
+import base64
 
-market_predict_list = []
+# 종목 데이터 크롤링
 def market_crawler(market):
     url = f"https://m.stock.naver.com/api/index/{market}/price?pageSize=30&page=1"
     response = requests.get(url)
-
     try:
         data = response.json()
     except json.JSONDecodeError:
         print("No data to decode")
         data = {}
-
     market_df = pd.DataFrame(data)
-    market_df = market_df[['localTradedAt', 'closePrice', 'compareToPreviousClosePrice', 'openPrice', 'highPrice', 'lowPrice']]
-    return market_df
+    return market_df[['localTradedAt', 'closePrice', 'compareToPreviousClosePrice', 'openPrice', 'highPrice', 'lowPrice']]
 
 def code_crawler(code):
     url = f"https://m.stock.naver.com/api/stock/{code}/price?pageSize=30&page=1"
     response = requests.get(url)
-
     try:
         data = response.json()
     except json.JSONDecodeError:
         print("No data to decode")
         data = {}
-
     code_df = pd.DataFrame(data)
-    code_df = code_df[['localTradedAt', 'closePrice', 'compareToPreviousClosePrice', 'openPrice', 'highPrice', 'lowPrice']]
-    return code_df
+    return code_df[['localTradedAt', 'closePrice', 'compareToPreviousClosePrice', 'openPrice', 'highPrice', 'lowPrice']]
 
+# 그래프 생성 및 이미지 저장
 def stock_graph(market, code, normalization):
     market_df = market_crawler(market)
-    market_price = market_df['closePrice']
-    market_price = market_price.str.replace(',', '').astype(float)
-
-    if (code != ''):
-        code_df = code_crawler(code)
-        code_price = code_df['closePrice']
-        code_price = code_price.str.replace(',', '').astype(float)
+    market_price = market_df['closePrice'].str.replace(',', '').astype(float)
 
     plt.figure(figsize=(8, 4))
-
     ax = plt.gca()
     ax.axes.xaxis.set_visible(False)
 
-
-    x_market = market_df['localTradedAt']
-    x_market = x_market[::-1]
+    # 시장 데이터 그래프
+    x_market = market_df['localTradedAt'][::-1]
     y_market = market_price.to_list()
     y_market_normalization = preprocessing.minmax_scale(y_market)
+    plt.plot(x_market, y_market_normalization if normalization == 'True' else y_market, label=market)
 
-    if (normalization == 'True'):
-        plt.plot(x_market, y_market_normalization, label=market)
-    else:
-        plt.plot(x_market, y_market, label=market)
-
-
-    if (code != ''):
-        x_code = code_df['localTradedAt']
-        x_code = x_code[::-1]
+    # 종목 데이터 그래프 (선택 사항)
+    if code:
+        code_df = code_crawler(code)
+        code_price = code_df['closePrice'].str.replace(',', '').astype(float)
+        x_code = code_df['localTradedAt'][::-1]
         y_code = code_price.to_list()
         y_code_normalization = preprocessing.minmax_scale(y_code)
-
-        if (normalization == 'True'):
-            plt.plot(x_code, y_code_normalization, label=code)
-        else:
-            plt.plot(x_code, y_code, label=code)
+        plt.plot(x_code, y_code_normalization if normalization == 'True' else y_code, label=code)
 
     plt.legend(loc=0)
 
-    static_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'static')
-    image_path = os.path.join(static_dir, 'images', 'graph.png')
+    # 그래프를 Base64로 인코딩
+    buffer = BytesIO()
+    plt.savefig(buffer, format='png')
+    buffer.seek(0)
+    image_png = buffer.getvalue()
+    buffer.close()
+    plt.close()
 
-    plt.savefig(image_path)
+    graph = base64.b64encode(image_png).decode('utf-8')
+    return graph
 
+# 상관계수 계산
 def coefficient(market, code):
     market_df = market_crawler(market)
-    market_price = market_df['closePrice']
-    market_price = market_price.str.replace(',', '').astype(float)
+    market_price = market_df['closePrice'].str.replace(',', '').astype(float)
+    y_market = preprocessing.minmax_scale(market_price.to_list())
 
-    if (code != ''):
+    if code:
         code_df = code_crawler(code)
-        code_price = code_df['closePrice']
-        code_price = code_price.str.replace(',', '').astype(float)
-        y_code = code_price.to_list()
-        y_code_normalization = preprocessing.minmax_scale(y_code)
-
-    y_market = market_price.to_list()
-    y_market_normalization = preprocessing.minmax_scale(y_market)
-
-    if (code != ''):
-        coef = np.corrcoef(y_market_normalization, y_code_normalization)[0, 1]*100
-        answer = f"{market}와 {code} 의 상관계수는 {coef:.2f}% 입니다."
+        code_price = code_df['closePrice'].str.replace(',', '').astype(float)
+        y_code = preprocessing.minmax_scale(code_price.to_list())
+        coef = np.corrcoef(y_market, y_code)[0, 1] * 100
+        return f"{market}와 {code}의 상관계수는 {coef:.2f}% 입니다."
     else:
-        answer = f"{code} 종목이 없어 {market} 와의 상관계수를 계산할 수 없습니다."
+        return f"{code} 종목이 없어 {market}와의 상관계수를 계산할 수 없습니다."
 
-    return answer
-
+# ARIMA 기반 가격 예측
 def market_price_predict(market):
     market_df = market_crawler(market)
-    market_price = market_df['closePrice']
-    market_price = market_price.str.replace(',', '').astype(float)
-
+    market_price = market_df['closePrice'].str.replace(',', '').astype(float)
     train_data = market_price[:-5]
-
-    # ARIMA 모델 설정 및 학습
-    model = ARIMA(train_data, order=(1, 1, 1))  # (p, d, q) 값을 적절히 설정
+    model = ARIMA(train_data, order=(1, 1, 1))
     model_fit = model.fit()
-
-    # 5일 예측
     forecast = model_fit.forecast(steps=5)
-
     return forecast.tolist()
 
 def code_price_predict(code):
-    if code != '':
+    if code:
         code_df = code_crawler(code)
-        code_price = code_df['closePrice']
-        code_price = code_price.str.replace(',', '').astype(float)
-
+        code_price = code_df['closePrice'].str.replace(',', '').astype(float)
         train_data = code_price[:-5]
-
-        model = ARIMA(train_data, order=(1, 1, 1))  # (p, d, q) 값을 적절히 설정
+        model = ARIMA(train_data, order=(1, 1, 1))
         model_fit = model.fit()
-
         forecast = model_fit.forecast(steps=5)
         return forecast.tolist()
-    else:
-        return ['?', '?', '?', '?', '?']
+    return ['?', '?', '?', '?', '?']
 
+# 예측 페이지 렌더링
 def prediction(request):
     market = 'KOSPI'
     code = ''
@@ -150,45 +119,16 @@ def prediction(request):
         code = request.GET.get('code', code)
         normalization = request.GET.get('normalization', normalization)
 
+    graph = stock_graph(market, code, normalization)
+    answer = coefficient(market, code)
+    market_forecast = market_price_predict(market)
+    code_forecast = code_price_predict(code)
 
-    # 여기에 그래프, 표, 예측 넣기
-    graph=stock_graph(market, code, normalization)
-    answer=coefficient(market, code)
-
-
-
-    html_response = f"""
-    <div class="container">
-        <a href="https://www.ktb.co.kr/trading/popup/itemPop.jspx" target="_blank"><img src="../static/images/Code.png" alt="코드 종목 조회"></a>
-        <p></p>
-        <form action="">
-            <select name="market">
-                <option value="KOSPI">KOSPI</option>
-                <option value="KOSDAQ">KOSDAQ</option>
-            </select>
-            <input type="text" name="code" placeholder="종목코드">
-            <select name="normalization">
-                <option value="False">기본</option>
-                <option value="True">정규화</option>
-            </select>
-            
-            <input type="submit" value="조회">
-        </form>
-        </div>
-        
-        <div class="graph" style="width:400px height:300px">
-        <img src="../static/images/graph.png" alt="그래프">
-        <p> {answer} </p>
-        
-        <p> {market} 예측 : {market_price_predict(market)} </p>
-    </div>
-
-    """
-
-    if (code != ''):
-        html_response = html_response + f"""
-        <p> {code} 예측 : {code_price_predict(code)} </p>
-        """
-
-    return HttpResponse(html_response)
-
+    return render(request, 'prediction.html', {
+        'graph': graph,
+        'answer': answer,
+        'market_forecast': market_forecast,
+        'code_forecast': code_forecast,
+        'market': market,
+        'code': code,
+    })
